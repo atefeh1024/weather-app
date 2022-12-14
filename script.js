4c94b2378b27089b69f2cfe80d5cf34272a119a8
atefeh <atefehkh1024@gmail.com>
2022-12-14 14:38:26 -0600

    created css and js file

---
+++ script.js
@@ -0,0 +1,66 @@
+let now = new Date();
+function formatDate(time) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let weekday = days[time.getDay()];
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${weekday} , ${hours}:${minutes}`;
+}
+let date = document.querySelector("#time");
+date.innerHTML = formatDate(now);
+
+function displayWeatherCondition(response) {
+  document.querySelector("h2").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "43acd35262a16faed5a0a96f55e152fd";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function replace(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city").value;
+  searchCity(city);
+}
+let searchcity = document.querySelector("#input-city");
+searchcity.addEventListener("submit", replace);
+
+function showPosition(position) {
+  console.log(position);
+  let apiKey = "43acd35262a16faed5a0a96f55e152fd";
+  let long = position.coords.longitude;
+  let lat = position.coords.latitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let button = document.querySelector("button");
+button.addEventListener("click", getCurrentPosition);
